e1f098cfadea7739b3d53fc9dbbee50dfd10683b
Laura van Gils <1209936@student.roc-nijmegen.nl>
2025-04-18 17:06:02 +0200

    Add files via upload

--- ovApparaat.java
+++ ovApparaat.java
@@ -1,27 +1,40 @@
 package ovSysteem;
 
+import java.util.*;
+
 public class ovApparaat
 {
-    static public double app = 3.0;
-    
-    public static void main(String[] args)
-    {	ovKaart ov = new ovKaart();
-	
-	// Laat ov saldo zien
-	System.out.println("Saldo in ov: €" + ov.saldo);
-	
-	// Als ov saldo meer is dan wat app nodig heeft
-	if (ov.saldo > app ) {
-		System.out.println("Ingecheckt!");
-		}
-	// Als ov saldo gelijk is aan wat app nodig heeft
-	else if (ov.saldo == app) {
-		System.out.println("Ingecheckt!");    
-		} 
-	// Als ov saldo minder is dan wat app nodig heeft
-	else if (ov.saldo < app) {
-		System.out.println("Error! Niet genoeg saldo.");    
-		} 
+    static Scanner scan = new Scanner(System.in);
 
+    public static void main(String[] args)
+    {
+	// Andere classes
+	ovKaart ov = new ovKaart();
+	incheckPoortje poort = new incheckPoortje();
+	Laadpunt opwaar = new Laadpunt(ov);
+	uitcheckPoortje uitcheck = new uitcheckPoortje();
+
+	// Ov plaats, naam en saldo
+	poort.setPlaats("Nijmegen");
+	ov.setName("Ov-kaart");
+	ov.setSaldo(7.0);
+
+	// Checken of ov is ingecheckt
+	poort.GeldigheidChecken(ov);
+
+	// Laat saldo zien als nodig
+	//System.out.println("Saldo in ov: €" + ov.saldo);
+	System.out.println("     ");
+
+	// Opwaardeer menu
+	opwaar.menu(ov, scan);
+
+	// Na opwaarderen checken of ov nog ingecheckt is
+	System.out.println("     ");
+	poort.GeldigheidChecken(ov);
+
+	// Uitchecken menu
+	System.out.println("     ");
+	uitcheck.vraagPlaats(ov, scan);
     }
 }
--- ovKaart.java
+++ ovKaart.java
@@ -2,7 +2,40 @@ package ovSysteem;
 
 public class ovKaart
 {
+    public static ovApparaat plaatsPoort = new ovApparaat();
+
     // Saldo van het ov
-    public double saldo = 5.0;
-    public String name = "ov-chipkaart";
+    public double saldo;
+    public String name;
+    public String plaats;
+    public boolean ingecheckt;
+
+    // Method voor de saldo
+    public void setSaldo(double newSaldo)
+    {
+	saldo = newSaldo;
+    }
+
+    public double getSaldo()
+    {
+	return saldo;
+    }
+
+    // Method voor de naam
+    public void setName(String newName)
+    {
+	name = newName;
+    }
+
+    // Method voor inchecken
+    public void setIngecheckt(Boolean newIngecheckt)
+    {
+	ingecheckt = newIngecheckt;
+    }
+
+    // Method voor de plaats
+    public void setPlaats(String newPlaats)
+    {
+	plaats = newPlaats;
+    }
 }
